fc138238f7305ef9dc0d1c4ad38ea287287aa81a
ykarocin <ysa@cin.ufpe.br>
2026-01-09 18:21:25 -0300

    :sparkles: feat: Importando rotas de usuários

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.routes import utils
+from app.api.routes import utils, users
 
 api_router = APIRouter()
 api_router.include_router(utils.router)
+api_router.include_router(users.router)
