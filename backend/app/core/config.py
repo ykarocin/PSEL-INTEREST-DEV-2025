3c1d24de282d96cb336dbc39241241eb25489a6b
ykarocin <ysa@cin.ufpe.br>
2026-01-09 10:43:12 -0300

    :card_file_box: raw: Adicionando variável SENTRY_DSN ao settings do config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -35,6 +35,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    SENTRY_DSN: Literal["local"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
         list[AnyUrl] | str, BeforeValidator(parse_cors)
