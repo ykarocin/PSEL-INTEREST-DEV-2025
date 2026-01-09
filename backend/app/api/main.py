0470ec3ee5af1b06f8fb68dd3cc32817d54c8000
ykarocin <ysa@cin.ufpe.br>
2026-01-09 20:16:06 -0300

    :sparkles: feat: Incluindo rotas de equipes

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.routes import utils, users
+from app.api.routes import utils, users, teams
 
 api_router = APIRouter()
 api_router.include_router(utils.router)
 api_router.include_router(users.router)
+api_router.include_router(teams.router)
