12cc814f6fb95069a11c720de05de10d58a9ddbd
ykarocin <ysa@cin.ufpe.br>
2026-01-09 20:11:35 -0300

    :sparkles: feat: Adicionando schemas de equipes

---
+++ backend/app/schemas/team.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from .user import UserRead
+
+class TeamCreate(BaseModel):
+    name: str = Field(max_length=100)
+    leader_id: int
+
+class TeamRead(BaseModel):
+    id: int
+    name: str
+    leader_id: int
+
+class TeamUpdate(BaseModel):
+    name: Optional[str] = Field(None, max_length=100)
+    leader_id: Optional[int] = Field(None)
