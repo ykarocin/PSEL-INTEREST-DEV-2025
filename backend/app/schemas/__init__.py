83b21368269d2a9b3f954e9541326fcd15a1683d
ykarocin <ysa@cin.ufpe.br>
2026-01-10 18:01:08 -0300

    :sparkles: feat: Adicionando imports do schema member

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,6 +1,7 @@
 # Imports para facilitar o uso dos schemas
 from .user import UserCreate, UserRead, UserUpdate
 from .team import TeamCreate, TeamRead, TeamUpdate
+from .member import MemberCreate, MemberRead
 
 __all__ = [
     "UserCreate",
@@ -9,4 +10,6 @@ __all__ = [
     "TeamCreate",
     "TeamRead",
     "TeamUpdate",
+    "MemberCreate",
+    "MemberRead",
 ]
