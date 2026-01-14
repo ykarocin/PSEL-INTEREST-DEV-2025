782bf4bc1bbb1259388902ed3cbd6409d184abe5
ykarocin <ysa@cin.ufpe.br>
2026-01-14 19:58:10 -0300

    :sparkles: feat: Adição de novas páginas ao front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QuestProvider } from './hooks/useQuestContext'
 import MainLayout from './layouts/MainLayout'
+import MainPage from './pages/MainPage'
+import UsersPage from './pages/UsersPage'
+import TeamsPage from './pages/TeamsPage'
+import TeamDetailPage from './pages/TeamDetailPage'
 import LandingPage from './pages/LandingPage'
 import QuestPage from './pages/QuestPage'
 import './App.css'
@@ -11,7 +15,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<MainLayout />}>
-            <Route index element={<LandingPage />} />
+            <Route index element={<MainPage />} />
+            <Route path="users" element={<UsersPage />} />
+            <Route path="teams" element={<TeamsPage />} />
+            <Route path="teams/:id" element={<TeamDetailPage />} />
+            <Route path="landing" element={<LandingPage />} />
             <Route path="quest/:id" element={<QuestPage />} />
           </Route>
         </Routes>
