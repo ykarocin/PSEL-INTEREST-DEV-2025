f4e9fdcd39b830638448bf20b4cd366c874357ef
ykarocin <ysa@cin.ufpe.br>
2026-01-14 20:57:38 -0300

    :sparkles: feat: Adicionando component sidebar ao layout principal

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Sidebar from '../components/Sidebar'
 
 const MainLayout: React.FC = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen)
+  }
+
   return (
     <div className="app-layout">
       <div className="stars"></div>
-      <Header />
-      <main className="main-content">
-        <Outlet />
-      </main>
+      <Header onToggleSidebar={toggleSidebar} />
+      <div className="content-wrapper">
+        <Sidebar isOpen={sidebarOpen} />
+        <main className="main-content">
+          <Outlet />
+        </main>
+      </div>
       <Footer />
     </div>
   )
 }
-
 export default MainLayout
