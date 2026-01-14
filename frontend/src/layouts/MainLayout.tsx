bd13d999ba22902b8826684c2fd2f3e6d090c596
ykarocin <ysa@cin.ufpe.br>
2026-01-14 20:46:19 -0300

    Revert ":sparkles: feat: Adicionando component sidebar ao MainLayout"
    This reverts commit 723204a6ab8c748563a37a6acab12da26083851c.

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,26 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Sidebar from '../components/Sidebar'
 
 const MainLayout: React.FC = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen)
-  }
-
   return (
     <div className="app-layout">
       <div className="stars"></div>
-      <Header onToggleSidebar={toggleSidebar} />
-      <div className="content-wrapper">
-        <Sidebar isOpen={sidebarOpen} />
-        <main className="main-content">
-          <Outlet />
-        </main>
-      </div>
+      <Header />
+      <main className="main-content">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   )
