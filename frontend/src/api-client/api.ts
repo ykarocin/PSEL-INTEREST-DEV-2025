f8c9165f6b49f30bfa06d83bccb99ea7af82b179
ykarocin <ysa@cin.ufpe.br>
2026-01-14 19:56:01 -0300

    :card_file_box: raw: Client TypeScript gerado pelo OpenAPI Generator

--- frontend/src/api-client/api.ts
+++ frontend/src/api-client/api.ts
@@ -107,6 +107,43 @@ export const MembersApiAxiosParamCreator = function (configuration?: Configurati
         },
         /**
          * 
+         * @summary List Members
+         * @param {number} teamId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        membersListMembers: async (teamId: number, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'teamId' is not null or undefined
+            assertParamExists('membersListMembers', 'teamId', teamId)
+            const localVarPath = `/api/v1/members/`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            if (teamId !== undefined) {
+                localVarQueryParameter['team_id'] = teamId;
+            }
+
+            localVarHeaderParameter['Accept'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
+         * 
          * @summary Remove Member
          * @param {number} teamId 
          * @param {number} userId 
@@ -167,6 +204,19 @@ export const MembersApiFp = function(configuration?: Configuration) {
         },
         /**
          * 
+         * @summary List Members
+         * @param {number} teamId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async membersListMembers(teamId: number, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<Array<MemberRead>>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.membersListMembers(teamId, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['MembersApi.membersListMembers']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+        /**
+         * 
          * @summary Remove Member
          * @param {number} teamId 
          * @param {number} userId 
@@ -200,6 +250,16 @@ export const MembersApiFactory = function (configuration?: Configuration, basePa
         },
         /**
          * 
+         * @summary List Members
+         * @param {number} teamId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        membersListMembers(teamId: number, options?: RawAxiosRequestConfig): AxiosPromise<Array<MemberRead>> {
+            return localVarFp.membersListMembers(teamId, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
          * @summary Remove Member
          * @param {number} teamId 
          * @param {number} userId 
@@ -225,6 +285,17 @@ export class MembersApi extends BaseAPI {
      */
     public membersAddMember(memberCreate: MemberCreate, options?: RawAxiosRequestConfig) {
         return MembersApiFp(this.configuration).membersAddMember(memberCreate, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * 
+     * @summary List Members
+     * @param {number} teamId 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     */
+    public membersListMembers(teamId: number, options?: RawAxiosRequestConfig) {
+        return MembersApiFp(this.configuration).membersListMembers(teamId, options).then((request) => request(this.axios, this.basePath));
     }
 
     /**
--- frontend/src/api-client/docs/MembersApi.md
+++ frontend/src/api-client/docs/MembersApi.md
@@ -5,6 +5,7 @@ All URIs are relative to *http://localhost*
 |Method | HTTP request | Description|
 |------------- | ------------- | -------------|
 |[**membersAddMember**](#membersaddmember) | **POST** /api/v1/members/ | Add Member|
+|[**membersListMembers**](#memberslistmembers) | **GET** /api/v1/members/ | List Members|
 |[**membersRemoveMember**](#membersremovemember) | **DELETE** /api/v1/members/{team_id}/{user_id} | Remove Member|
 
 # **membersAddMember**
@@ -48,6 +49,57 @@ No authorization required
 ### HTTP request headers
 
  - **Content-Type**: application/json
+ - **Accept**: application/json
+
+
+### HTTP response details
+| Status code | Description | Response headers |
+|-------------|-------------|------------------|
+|**200** | Successful Response |  -  |
+|**422** | Validation Error |  -  |
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
+
+# **membersListMembers**
+> Array<MemberRead> membersListMembers()
+
+
+### Example
+
+```typescript
+import {
+    MembersApi,
+    Configuration
+} from './api';
+
+const configuration = new Configuration();
+const apiInstance = new MembersApi(configuration);
+
+let teamId: number; // (default to undefined)
+
+const { status, data } = await apiInstance.membersListMembers(
+    teamId
+);
+```
+
+### Parameters
+
+|Name | Type | Description  | Notes|
+|------------- | ------------- | ------------- | -------------|
+| **teamId** | [**number**] |  | defaults to undefined|
+
+
+### Return type
+
+**Array<MemberRead>**
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+ - **Content-Type**: Not defined
  - **Accept**: application/json
 
 
