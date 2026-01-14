398723ec50ad6af3f3267882141c68e5f49e58ef
ykarocin <ysa@cin.ufpe.br>
2026-01-14 18:41:22 -0300

    :sparkles: feat: Adicionando erro do tipo regra de negócio

--- backend/app/exceptions.py
+++ backend/app/exceptions.py
@@ -1,2 +1,11 @@
-class NotFoundError(Exception):
+class DomainError(Exception):
+    def __init__(self, error_code: str, message: str = ""):
+        self.error_code = error_code
+        self.message = message
+        super().__init__(message)
+
+class NotFoundError(DomainError):
     """Entidade não encontrada"""
+
+class BusinessRuleError(DomainError):
+    pass    
