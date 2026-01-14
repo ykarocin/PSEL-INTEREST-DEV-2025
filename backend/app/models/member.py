c4feadaa7cc16b2b5fac50c48d7643c5299aabda
ykarocin <ysa@cin.ufpe.br>
2026-01-14 18:56:23 -0300

    :sparkles: feat: Permitindo que usuários sejam deletados mesmo que façam parte de uma equipe

--- backend/app/models/member.py
+++ backend/app/models/member.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Field, Relationship
 
 class TeamMember(SQLModel, table=True):
     team_id: int = Field(foreign_key="team.id", primary_key=True)
-    user_id: int = Field(foreign_key="user.id", primary_key=True, unique=True)  # Unicidade de membro
+    user_id: int = Field(foreign_key="user.id", primary_key=True, unique=True, ondelete="CASCADE")  # Unicidade de membro
 
     # Relacionamentos
     team: "Team" = Relationship(back_populates="members")
